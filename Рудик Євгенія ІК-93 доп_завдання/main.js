7b74e32b8c10a31da8d6b8a538facfe1c250bf06
local-dwarf <54265697+local-dwarf@users.noreply.github.com>
2021-03-11 00:50:21 +0200

    Add files via upload

---
+++ Рудик Євгенія ІК-93 доп_завдання/main.js
@@ -0,0 +1,8 @@
+const { min, abs, summ } = require('math')
+
+console.log(`Array:[7,1,3,22,6,-17,9,0]`)
+console.log(`min in array: ${min([7, 1, 3, 22, 6, -17, 9, 0])}`)
+console.log(`Number : -24`)
+console.log(`abs: ${abs(-24)}`)
+console.log(`Array:[43,7,15,8,23]`)
+console.log(`summ of all: ${summ(43, 7, 15, 8, 23)}`)
