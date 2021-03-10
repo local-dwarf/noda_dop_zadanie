ae4f2752a40ec8940abe65c684525e85096b0cab
local-dwarf <54265697+local-dwarf@users.noreply.github.com>
2021-03-11 00:49:58 +0200

    Delete Рудик Євгенія ІК-93 доп_завдання directory

--- Рудик Євгенія ІК-93 доп_завдання/main.js
+++
@@ -1,5 +0,0 @@
-const { min, abs, summ } = require('math')
-
-console.log(`min(): ${min([7, 1, 3, 22, 6, -17, 9, 0])}`)
-console.log(`abs(): ${abs(-24)}`)
-console.log(`summ(): ${summ(43, 7, 15, 8, 23)}`)
